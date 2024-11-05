0d9a8c7588f0dbc921ec677c4a4d9b887960c848
himeko24 <stefanusandrian4@gmail.com>
2024-11-05 19:08:13 +0700

    added for week 8

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getUsers } from './services/axios';
+import { useEffect } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const getAllUsers = () => {
+        getUsers().then((res) => {
+            if (res.status === 200) {
+                console.log(res.data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        getAllUsers();
+    }, []);
+
+    const data = {
+        title: "Title",
+        body: "Body",
+        userId: 1,
+    };
+
+    const handlePostData = () => {
+        postData(data).then((res) => {
+            console.log(res);
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text>Stefanus Andrian Leoandi - 00000055662</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
